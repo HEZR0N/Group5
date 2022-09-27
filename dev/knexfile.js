64e1314a3fed372c106005e08719db642c8067f2
Carlos <72821925+Seazie@users.noreply.github.com>
2022-09-27 16:26:36 -0500

    Revert "Foundational Code"

--- dev/knexfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    development: {
-        client: 'pg',
-        connection : {
-            // db_type://user:password@IP:PORT/database_name
-            connectionString: `postgres://postgres:postgres@127.0.0.1:5433/testdb`
-        }
-    }
-}
