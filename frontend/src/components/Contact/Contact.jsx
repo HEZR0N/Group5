6cec94c5fabbe14d12be2ce22bcd53329589cda2
martinsanabia <martin.sanabia@my.utsa.edu>
2022-11-09 16:21:19 -0600

    ola3

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -4,9 +4,7 @@ import './Contact.css'
 const Contact = () => {
   return (
     <>
-    <div id="nfound">
-    <p class="nftext">The page you are looking for is in another castle</p>
-    </div>
+    <p>Contact</p>
     </>
   )
 }
