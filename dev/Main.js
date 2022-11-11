dcfad80c71dea3b27a3f648091a40c801a433e6a
HEZR0N <99786488+HEZR0N@users.noreply.github.com>
2022-11-10 18:21:46 -0600

    use cors in dev/main

--- dev/Main.js
+++ dev/Main.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 var bodyParser = require('body-parser');
+var cors = require("cors");
 
 const users = require('./routes/users');
+const items = require('./routes/items');
 
 app.get('/', function(req,res){
     res.json({'hello': 'world'});
 });
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/users', users);
+app.use('/api/items', items);
 
 app.listen(5000);
